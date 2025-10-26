de3d73d81e951fa3a9024b6223e4a04389199e12
pleaobraga <pleao.braga@gmail.com>
2025-10-26 10:29:54 -0300

    feat: create db

--- apps/api/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { defineAbilitiesFor } from '@repo/auth'
-
-const ability = defineAbilitiesFor({ role: 'MEMBER', id: 'test' })
-
-const userCanInvite = ability.can('invite', 'User')
-
-console.log('User can invite:', userCanInvite)
